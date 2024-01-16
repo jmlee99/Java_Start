926b00d3dcfdbee4a5be617bce803c20a804a6b7
jjam <ljm9017@naver.com>
2024-01-16 14:17:31 +0900

    Commit 1/16

--- src/HelloJava1.java
+++ src/HelloJava1.java
@@ -1,4 +1,4 @@
-public class HelloJava {
+public class HelloJava1 {
     public static void main(String[] args){
         System.out.println("hello java");
         System.out.println("Hello java1");
