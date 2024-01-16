59c4e6017af419375a4f690c22aab8cab30bbc19
jjam <ljm9017@naver.com>
2024-01-16 14:04:03 +0900

    Commit 1/16

---
+++ src/HelloJava.java
@@ -0,0 +1,26 @@
+public class HelloJava {
+    public static void main(String[] args){
+        System.out.println("hello java");
+        System.out.println("Hello java1");
+        System.out.println("Hello java2");
+    }
+}
+// 주의 : 자바 언어는 대소문자를 구분한다. 대소문자가 다르면 오류가 발생 가능
+/*
+* public class HelloJava
+* HelloJava를 클래스라고 한다. 클래스의 개념을 학습해야 이해할 수 있다. 클래스는 뒤에서 학습
+* 지금은 단순히 HelloJava.java라는 파일을 만들었다고 이해하면 된다.
+* 파일명과 클래스 이름이 같아야 한다.
+* {} 블록을 사용해서 클래스의 시작과 끝을 나타낸다
+*
+* public static void main(String[] args)
+* main 메서드라 한다. 함수, 메서드의 개념을 학습해야 이해할 수 있다. 함수, 메서드는 뒤에서 학습
+* 자바는 main(String[] args) 메서드를 찾아서 프로그램을 시작한다.
+* 지금은 단순히 main은 프로그램의 시작점이라고 이해하면 된다.
+* {} 블록을 사용해서 메서드의 시작과 끝을 나타낸다.
+*
+* System.out.println("Hello java");
+* System.out.println(); 값을 콘솔에 출력하는 기능이다.
+* "hello java" 자바는 문자열을 사용할 때 ""(쌍따옴표)를 사용한다. 쌍따옴표 사이에 원하는 문자열을 감싸면 된다.
+* ; 자바는 세미콜론으로 문장을 구분한다. 문장이 끝나면 세미콜론을 필수로 넣어주어야 한다.
+* */
