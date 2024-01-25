88b2265ca5f967e5eeace1bd018e2ec30946dd2a
jjam <ljm9017@naver.com>
2024-01-25 22:44:37 +0900

    Commit 1/25, Scanner 및 Input 문제풀이

---
+++ src/scanner/ex/ScannerWhileEx3.java
@@ -0,0 +1,13 @@
+package scanner.ex;
+import java.util.Scanner;
+public class ScannerWhileEx3 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요:");
+        }
+
+
+    }
+}
